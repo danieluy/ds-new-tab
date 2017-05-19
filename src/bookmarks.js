90ad0a771ac0ef0cf6e847cf9917927a15338e70
danielsosauy <danielsosa.dev@gmail.com>
2017-05-19 11:04:40 -0300

    Adds Material-UI for React

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -1,1 +1,16 @@
-export default {}
+"use strict";
+
+function getBookmarks() {
+  return new Promise((resolve, reject) => {
+    chrome.bookmarks.getTree((bookmarks_tree) => {
+      return resolve({
+        bookmarks_bar: bookmarks_tree[0].children[0].children,
+        other_bookmarks: bookmarks_tree[0].children[1].children
+      });
+    });
+  })
+}
+
+module.exports = {
+  get: getBookmarks
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,88 @@
 import React, { Component } from 'react';
 
-import Bookmarks from '../bookmarks';
+import BookmarksProvider from '../bookmarks';
 
-import Header from './header';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import Chip from 'material-ui/Chip';
+
+
+// Needed for onTouchTap ///////////////////////////////////////////////////////////
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+////////////////////////////////////////////////////////////////////////////////////
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      bookmarks: []
+      bookmarks: {
+        bookmarks_bar: [],
+        other_bookmarks: []
+      },
+      drawer_open: false
     }
   }
   componentWillMount() {
-    Bookmarks.get().then(bookmarks => {
+    BookmarksProvider.get().then(bookmarks => {
       this.setState({
-        bookmarks: bookmarks
+        bookmarks: {
+          bookmarks_bar: bookmarks.bookmarks_bar,
+          other_bookmarks: bookmarks.other_bookmarks
+        }
       })
     })
   }
+  toggleDrawer() {
+    this.setState({
+      drawer_open: !this.state.drawer_open
+    })
+  }
+  doNothing() {
+    console.log('Nothing done!');
+  }
   render() {
+    const styles = {
+      chip: {
+        margin: 4,
+      },
+      wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+    };
     return (
-      <div>
-        <Header />
-        <BookmarksBar bookmarks={this.state.bookmarks} />
-      </div>
+      <MuiThemeProvider>
+        <div>
+
+          <AppBar
+            title="DS Newtab"
+            onLeftIconButtonTouchTap={this.toggleDrawer.bind(this)}
+          />
+
+          <Drawer
+            docked={false}
+            width={250}
+            open={this.state.drawer_open}
+            onRequestChange={(drawer_open) => this.setState({ drawer_open })}
+          >
+            <MenuItem onTouchTap={this.doNothing}>Menu Item 1</MenuItem>
+            <MenuItem onTouchTap={this.doNothing}>Menu Item 2</MenuItem>
+          </Drawer>
+
+          <div>
+            {this.state.bookmarks.bookmarks_bar.map((bookmark, i) => {
+              console.log(bookmark);
+              return <Chip key={i} style={styles.chip}>{bookmark.title ? bookmark.title : bookmark.url}</Chip>
+            })}
+          </div>
+
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
